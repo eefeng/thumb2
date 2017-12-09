b86edfa40c7d63840dc91cb6c7e615daf89901e7
Feng <eefeng@vip.qq.com>
2017-12-09 23:13:33 +0800

    thumb ready

---
+++ controller/initController.es.js
@@ -0,0 +1,13 @@
+import index from './indexController.js';
+
+const controllerInit = {
+    init(app, router) {
+        app.use(
+            router( _ => {
+                _.get('/index/index', index.index())
+            })
+        );
+    }
+};
+
+export default controllerInit;
